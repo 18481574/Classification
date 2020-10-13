d5500067872d7bd09cedb474373ac416dfbb3cd0
Chaos <18481574@life.hkbu.edu.hk>
2020-10-13 23:38:51 +0800

    update files

--- models/GraphTV.py
+++ models/GraphTV.py
@@ -1,44 +1,60 @@
 import numpy as np 
 import torch
 import torch.nn as nn
-import torch.sparse.FloatTensor as sparse
+import torch.sparse as sparse
+
+from sklearn.neighbors import NearestNeighbors
 
 __all__ = ['GraphTV', ]
 
+
+# Example:
+# 	layer = GraphTV(n, alpha)
+#   feature = feature(xs)
+# 	output = model(xs) 
+# 	W = Graph._get_W(feature)
+# 	loss_TV = layer(output, W) 
+#   loss_TV.backward()...
+# 
 class GraphTV(nn.Module):
-    def __init__(self, n=100, W=None):
-        super(GraphTV, self).__init__():
-            self.n = n
-            self.W = W
+    def __init__(self, n=100, alpha=1.):
+        super(GraphTV, self).__init__()
+        self.n = n
+        self.alpha = alpha 
 
-    def forward(self, x):
+    def forward(self, x, W):
+
+    	Wx = W.matmul(x)
+    	norm_ = Wx.norm(dim=1)
+
+    	return self.alpha *  torch.sum(norm_)
 
 
-
-    def _upd_W(self, W):
-        self.W = W
+    def _upd(self, n = 100, alpha=1.0):
+        self.n = n
+        self.alpha = alpha
 
     @staticmethod
-    def _get_W(x, n_Neigb=15, n_sig=8):
+    def _get_W(x:torch.Tensor, n_Neigbr=15, n_sig=8):
         N, d = x.shape # N: number of samples, d: the dimension of the sample data
         if N < 2:
             raise ValueError('The number of sample must more than 1!!!')
         
-        n_Neigb = min(N, n_Neigb)
-        n_sig = min(n_sig, n_Neigb)
+        n_Neigb = min(N, n_Neigbr)
+        n_sig = min(n_sig, n_Neigbr)
 
         # Calculate the distance
         X = x.numpy()
-        nbrs = NearestNeighbors(n_neighbors=n_Neigb, algorithm='ball_tree').fit(X)
+        nbrs = NearestNeighbors(n_neighbors=n_Neigbr, algorithm='ball_tree').fit(X)
 
         dis, idx = nbrs.kneighbors(X)
-        dis2 = dis**2
+        dis2 = np.exp(- dis**2 / dis[:, n_sig:n_sig+1] / 2 )
 
-        M = (n_Neigb - 1) * N
-        idx_i = torch.cat((torch.LongTensor(range(M)), torch.LongTensor(range(M)) ))
-        idx_j = torch.cat((torch.LongTenor([idx[:,1:].repeat(n_Neigb-1), axis=0]), torch.LongTenor() ))
+        M = (n_Neigbr - 1) * N
+        idx_i = torch.cat( (torch.LongTensor(range(M)), torch.LongTensor(range(M)), ) )
+        idx_j = torch.cat( (torch.LongTensor(idx[:, 0].repeat(n_Neigbr-1)), torch.LongTensor(idx[:,1:].repeat(1)), ) )
         
-
-        v = torch.sqrt(v) 
+        v = torch.cat( (torch.FloatTensor(dis2[:, 1:] ).view( -1), - torch.FloatTensor(dis2[:, 1:]).view(-1), ) )
+        print(idx_i.shape, idx_j.shape, v.shape)
         
-        return sparse(torch.cat(idx_i.view(-1,1), idx_j), v)
+        return sparse.FloatTensor(torch.cat( (idx_i.view(1, -1), idx_j.view(1, -1)) ), v, torch.Size([M, N]))
